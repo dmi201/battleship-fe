3149f4530c6d93e16f9a61d901bf0a1883118ab0
Mihai <iuliandinu201@gmail.com>
2024-09-03 12:14:31 +0300

    add layouts, components, routes and pages

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import GridCell from "./GridCell";
+
+const GameBoard: React.FC = () => {
+  return (
+    <>
+      <div>Game Board</div>
+      <GridCell />
+    </>
+  );
+};
+
+export default GameBoard;
