103512b04ed0bd399e4e2201fd3e79c0c4075e71
Mihai <iuliandinu201@gmail.com>
2024-09-03 17:30:26 +0300

    add game logic, add GameInfo component

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,4 +1,4 @@
-export type CellState = "empty" | "hit" | "miss" | "ship";
+export type CellState = "empty" | "hit" | "miss" | "ship" | "sunk";
 
 // Represents the state of the game board as a 2D array of cell states
 export type BoardState = CellState[][];
@@ -7,6 +7,7 @@ export type BoardState = CellState[][];
 export type GameState = {
   boardState: BoardState; // Current state of the game board
   playerHits: [number, number][]; // Array of positions where the player has hit
+  playerMisses: [number, number][]; // Array of positions where the player has miss
   shipsSunk: string[]; // Array of ship names that have been completely sunk
   gameOver: boolean; // Boolean indicating whether the game is over
   shipTypes: string[][];
