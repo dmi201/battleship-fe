d3c875a696bba9db74eea923562630a6426e1034
Mihai <iuliandinu201@gmail.com>
2024-09-04 11:13:09 +0300

    add header and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { UserProvider } from "@/context/UserContext";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <UserProvider>
         {" "}
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Header />
+          {children}
+          <Footer />
+        </body>
       </UserProvider>
     </html>
   );
